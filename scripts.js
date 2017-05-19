790c16d7ca99bb74f94be1e8e3dcedd739fbf87b
Manoela Silverio Gomes <Manoela@Air-de-Manoela.home>
2017-05-19 01:14:31 -0300

    Retirado header e cores de fundo; adicionada imagem de exemplo no fundo inicial (verificar no .css o local da substituicao da nova imagem); criado banner rotativo na ultima sessao

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-// teste
+var banners = document.querySelectorAll('.slide1,.slide2,.slide3');
+var tatuadores = document.querySelectorAll('.tatuador1,.tatuador2,.tatuador3');
+var bannerAtivo = 0;
+var velocidade = 5000; // 5 segundos a principio
+
+function avancarBanner(){
+    banners[bannerAtivo].classList.remove('active');
+    tatuadores[bannerAtivo].classList.remove('active');
+
+    bannerAtivo++;
+
+    if(bannerAtivo >= banners.length){
+        bannerAtivo = 0;
+    }
+
+    banners[bannerAtivo].classList.add('active');
+    tatuadores[bannerAtivo].classList.add('active');
+}
+setInterval(avancarBanner,velocidade);
