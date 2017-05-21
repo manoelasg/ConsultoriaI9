a306e1955343c7c326d6e04c2c0ada03c94de0ed
Manoela Silverio Gomes <Manoela@Air-de-Manoela.home>
2017-05-21 20:38:23 -0300

    Evolucao com uma parte do Gui e uma parte da Lilia

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
+// Buscando elementos para a lógica
 var banners = document.querySelectorAll('.slide1,.slide2,.slide3');
 var tatuadores = document.querySelectorAll('.tatuador1,.tatuador2,.tatuador3');
+
+// Variáveis globais do banner
 var bannerAtivo = 0;
 var velocidade = 5000; // 5 segundos a principio
 
+// Lógica do Banner
 function avancarBanner(){
     banners[bannerAtivo].classList.remove('active');
     tatuadores[bannerAtivo].classList.remove('active');
@@ -16,4 +20,5 @@ function avancarBanner(){
     banners[bannerAtivo].classList.add('active');
     tatuadores[bannerAtivo].classList.add('active');
 }
+
 setInterval(avancarBanner,velocidade);
